6af0d107a39305ddfee326169c1ca032848d03cb
andyosyndoh <andyovvo8@gmail.com>
2025-05-25 22:01:11 +0300

    chore added login user view

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.views.generic import CreateView, TemplateView
+from django.contrib.auth.hashers import check_password
 
 from .forms import CustomerSignUpForm, CompanySignUpForm, UserLoginForm
 from .models import User, Company, Customer
@@ -41,4 +42,20 @@ class CompanySignUpView(CreateView):
 
 
 def LoginUserView(request):
-    pass
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            try:
+                user = User.objects.get(email=email)
+                if check_password(password, user.password):
+                    login(request, user)
+                    return redirect('/')
+                else:
+                    form.add_error(None, 'Invalid email or password')
+            except User.DoesNotExist:
+                form.add_error(None, 'Invalid email or password')
+    else:
+        form = UserLoginForm()
+    return render(request, 'users/login.html', {'form': form})
