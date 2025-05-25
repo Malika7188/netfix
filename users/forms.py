4dd0318c12eb23c7f30acfc4c4c863cc0f9dba23
andyosyndoh <andyovvo8@gmail.com>
2025-05-25 22:02:59 +0300

    chore: modified user forms

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, authenticate
 from django.db import transaction
 from django.core.exceptions import ValidationError
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 from .models import User, Company, Customer
 
@@ -17,22 +19,113 @@ def validate_email(value):
             value + " is already taken.")
 
 
+def validate_age(value):
+    today = date.today()
+    age = relativedelta(today, value).years
+    if age < 18:
+        raise ValidationError('You must be at least 18 years old to register.')
+
+
 class CustomerSignUpForm(UserCreationForm):
-    pass
+    email = forms.EmailField(
+        label='Email Address',
+        widget=forms.EmailInput(attrs={'placeholder': 'Enter Email'})
+    )
+    birth = forms.DateField(
+        label='Date of Birth',
+        widget=DateInput(),
+        required=True,
+        validators=[validate_age],
+        help_text='You must be at least 18 years old'
+    )
+    
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+        labels = {
+            'username': 'Username',
+            'password1': 'Password',
+            'password2': 'Confirm Password'
+        }
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter Username'
+            }),
+            'password1': forms.PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter Password'
+            }),
+            'password2': forms.PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Confirm Password'
+            })
+        }
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_customer = True
+        user.save()
+        customer = Customer.objects.create(user=user, birth=self.cleaned_data.get('birth'))
+        return user
 
 
 class CompanySignUpForm(UserCreationForm):
-    pass
+    email = forms.EmailField(
+        label='Email Address',
+        widget=forms.EmailInput(attrs={'placeholder': 'Enter Email'})
+    )
+    field = forms.ChoiceField(
+        label='Service Field',
+        choices=Company._meta.get_field('field').choices,
+        help_text='Select your main service area'
+    )
+    
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+        labels = {
+            'username': 'Username',
+            'password1': 'Password',
+            'password2': 'Confirm Password'
+        }
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter Username'
+            }),
+            'password1': forms.PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter Password'
+            }),
+            'password2': forms.PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Confirm Password'
+            })
+        }
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_company = True
+        user.save()
+        company = Company.objects.create(user=user, field=self.cleaned_data.get('field'))
+        return user
 
 
 class UserLoginForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super(UserLoginForm, self).__init__(*args, **kwargs)
 
-    email = forms.EmailField(widget=forms.TextInput(
-        attrs={'placeholder': 'Enter Email'}))
+    email = forms.EmailField(
+        label='Email Address',
+        widget=forms.TextInput(attrs={'placeholder': 'Enter Email'})
+    )
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={'placeholder': 'Enter Password'}))
+        label='Password',
+        widget=forms.PasswordInput(attrs={'placeholder': 'Enter Password'})
+    )
 
     def __init__(self, *args, **kwargs):
         super(UserLoginForm, self).__init__(*args, **kwargs)
