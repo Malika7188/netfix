89afa63b38de4ea4a7aef8516b411b89b5681621
masman <asmanodeny@gmail.com>
2025-05-26 12:21:11 +0300

    addded services fields

--- main/models.py
+++ main/models.py
@@ -1,2 +1,17 @@
 from django.db import models
 from users.models import Company, Customer
+
+class Service(models.Model):
+    FIELD_CHOICES = [
+        ('Air Conditioner', 'Air Conditioner'),
+        ('Carpentry', 'Carpentry'),
+        ('Electricity', 'Electricity'),
+        ('Gardening', 'Gardening'),
+        ('Home Machines', 'Home Machines'),
+        ('Housekeeping', 'Housekeeping'),
+        ('Interior Design', 'Interior Design'),
+        ('Locks', 'Locks'),
+        ('Painting', 'Painting'),
+        ('Plumbing', 'Plumbing'),
+        ('Water Heaters', 'Water Heaters'),
+    ]
