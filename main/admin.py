aa11c1e5785aa943256e5cc18052891e8651a952
masman <asmanodeny@gmail.com>
2025-05-26 12:19:42 +0300

    feat: added admin section for registering requested services fields

--- main/admin.py
+++ main/admin.py
@@ -6,3 +6,9 @@ class ServiceAdmin(admin.ModelAdmin):
     list_display = ('name', 'field', 'price_per_hour', 'date_created', 'company')
     search_fields = ('name', 'field', 'company__username')
     list_filter = ('field', 'date_created')
+
+@admin.register(RequestedService)
+class RequestedServiceAdmin(admin.ModelAdmin):
+    list_display = ('service', 'customer', 'address', 'service_time', 'total_cost', 'date_requested')
+    search_fields = ('service__name', 'customer__username', 'address')
+    list_filter = ('date_requested', 'service__field')
