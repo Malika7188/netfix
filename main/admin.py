0da931fad3e7c3a4e2e23fe20683dabeb099bd73
masman <asmanodeny@gmail.com>
2025-05-26 12:19:00 +0300

    feat: added admin section for registering fields

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Service, RequestedService
 
-# Register your models here.
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'field', 'price_per_hour', 'date_created', 'company')
+    search_fields = ('name', 'field', 'company__username')
+    list_filter = ('field', 'date_created')
