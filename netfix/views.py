29d3f0f0bdc8335174e256f9b84702d8cc9b0e3a
masman <asmanodeny@gmail.com>
2025-05-26 12:13:54 +0300

    feat: added views.py

--- netfix/views.py
+++ netfix/views.py
@@ -1,21 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
-from users.models import User, Company
-from services.models import Service
+from users.models import User, Company, Customer
+from services.models import Service, RequestedService
 
 
 def home(request):
     return render(request, 'users/home.html', {'user': request.user})
 
 
-def customer_profile(request):
-    pass
+@login_required
+def customer_profile(request, name):
+    # Fetch the customer user and all their requested services
+    user = get_object_or_404(User, username=name)
+    customer = get_object_or_404(Customer, user=user)
+    requested_services = RequestedService.objects.filter(
+        customer=customer).order_by("-date_requested")
+    
+    return render(request, 'users/customer_profile.html', {
+        'user': user,
+        'requested_services': requested_services
+    })
 
 
+@login_required
 def company_profile(request, name):
-    # fetches the company user and all of the services available by it
-    user = User.objects.get(username=name)
+    # Fetch the company user and all of their services
+    user = get_object_or_404(User, username=name)
+    company = get_object_or_404(Company, user=user)
     services = Service.objects.filter(
-        company=Company.objects.get(user=user)).order_by("-date")
-
-    return render(request, 'users/profile.html', {'user': user, 'services': services})
+        company=company).order_by("-date")
+    
+    return render(request, 'users/company_profile.html', {
+        'user': user,
+        'company': company,
+        'services': services
+    })
